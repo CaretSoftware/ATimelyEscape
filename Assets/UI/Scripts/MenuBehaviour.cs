0807355da4cd1399ca7066c253a532fe992bb253
MVBNV700 <vikstrom18noah@gmail.com>
2022-10-12 20:09:25 +0200

    Updated CanvasMainMenu Prefab
    Har nu implementerat med knapp-funktionen att spelaren kan växla mellan Credits, Settings och Start menyn

--- Assets/UI/Scripts/MenuBehaviour.cs
+++ Assets/UI/Scripts/MenuBehaviour.cs
@@ -5,23 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class MenuBehaviour : MonoBehaviour
 {
-
     // Method to load a scene by insert the index of wished scene presented in buildsettings
     public void SelectScene(int sceneIndex)
     {
-        if(sceneIndex < 0 && sceneIndex > SceneManager.sceneCount)
+        if(sceneIndex < 0 || sceneIndex > SceneManager.sceneCount)
         {
-            Debug.Log("Error: Not valid SceneIndex");
+            Debug.Log("Error: Not valid SceneIndex: " + sceneIndex);
         }
         else
         {
-            SceneManager.LoadScene(sceneIndex); // Later install LoadSceneAsync() for imporved loading experience with loadingScreen
+            SceneManager.LoadScene(sceneIndex); // Later program LoadSceneAsync() for imporved loading experience with loadingScreen
         }
     }
+
+
 
     // Method to quit the application anytime
     public void QuitGame()
     {
+        Debug.Log("Info: Quit button has been Pressed");
+
         Application.Quit();
     }
 }
