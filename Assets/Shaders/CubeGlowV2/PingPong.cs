578213960b78b4ef272c796cd8053c57359636a8
CaretSoftware <pbergsten@me.com>
2022-10-14 22:37:58 +0200

    Cube shader graph

--- Assets/Shaders/CubeGlowV2/PingPong.cs
+++ Assets/Shaders/CubeGlowV2/PingPong.cs
@@ -3,11 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using Ping = System.Net.NetworkInformation.Ping;
 
 public class PingPong : MonoBehaviour {
-   private Material _mat;
+   private static readonly int VoronoiAngleOffset = Shader.PropertyToID("_VoronoiAngleOffset");
+   [SerializeField] private Material mat;
+   private MaterialPropertyBlock mpb;
+   private MeshRenderer mr;
+   private float _t;
 
-   private void Start() {
-      throw new NotImplementedException();
+   private void Awake() {
+      mpb = new MaterialPropertyBlock();
+      mr = GetComponent<MeshRenderer>();
+   }
+
+
+   private void Update() {
+      _t = Mathf.Sin(Time.time);
+      VoronoiOffset(_t);
+   }
+
+   private void VoronoiOffset(float t) {
+      float offset = Mathf.Lerp(45, 50, t);
+      mpb.SetFloat(VoronoiAngleOffset, offset);
+      mr.SetPropertyBlock(mpb);
    }
 }
