0260d17af6ea12790ced12f53c1b30640e340b50
CaretSoftware <pbergsten@me.com>
2022-10-14 21:16:46 +0200

    Cube Glow ShaderGraph
    My autocomplete stopped working all of a sudden ?
    Got it going again with some effort.

---
+++ Assets/Shaders/CubeGlowV2/PingPong.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PingPong : MonoBehaviour {
+   private Material _mat;
+
+   private void Start() {
+      throw new NotImplementedException();
+   }
+}
