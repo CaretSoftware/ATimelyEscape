93cde30350383629ba35fa389644f0436abec9bc
CaretSoftware <pbergsten@me.com>
2022-10-22 03:06:35 +0200

    Dummy Controller Improvements
    Math! Ho, Ha! What is it good for? — Absolutely nothing!

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,18 +7,44 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour {
 	[SerializeField] private Transform follow;
 	private Transform _transform;
+	private Transform _camera;
+	private Vector3 _currentVelocity;
+	[SerializeField] private float smoothness = 0.2f;
+	[SerializeField] private float maxVelocity = 1.0f;
+	[SerializeField] private float largestMagnitude = .07f;
 
+	public float maxSpeed;
+	public float smoothTime;
 	private void Awake() {
 		if (follow == null)
 			follow = FindObjectOfType<CharacterAnimationController>().transform;
 		
 		_transform = transform;
+		_camera = GetComponentInChildren<Camera>().transform; 
 		_transform.position = follow.position;
 	}
 
+	public float superSonic = 5.0f;
+	public float superSmooth = 0.01f;
+
 	private void LateUpdate() {
-		_transform.position = follow.position;	// TODO smoothDamp, turn CameraFollow into CineMachine
+		Transform thisTransform = _transform;
+		Vector3 position = thisTransform.position;
+		Vector3 followPosition = follow.position;
+		Vector3 direction = followPosition - position;
+		direction = _camera.InverseTransformDirection(direction);
+
+		float magnitude = direction.ProjectOnPlane().z;
+		
+		float t = Mathf.InverseLerp(0.0f, largestMagnitude, magnitude);
+		
+		maxSpeed = Mathf.Lerp(superSonic, maxVelocity, t);
+		smoothTime = Mathf.Lerp(superSmooth,smoothness, t);
+
+		Vector3 newPosition = Vector3.SmoothDamp(position, followPosition, ref _currentVelocity, smoothTime, maxSpeed);
+		
+		thisTransform.position = newPosition;
 	}
 
-	public void SetFollowTransform(Transform transform) => follow = transform;
+	public void SetFollowTransform(Transform newTransform) => follow = newTransform;
 }
