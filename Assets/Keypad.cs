4acf32ada6b58f3aa54404efb9eee58cb5bc167c
Ossian Däckfors <ossian.dackfors@gmail.com>
2022-10-25 15:08:57 +0200

    KeyPadInteration first draft

---
+++ Assets/Keypad.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Text;
+
+public class Keypad : MonoBehaviour
+{
+    [SerializeField] private int maxDigits;
+    [SerializeField] private int combination;
+    [SerializeField] private TextMeshProUGUI screen;
+    public void KeypadDigitInput(int number)
+    {
+        Debug.Log("Hej");
+        if (screen.text.ToCharArray().Length < maxDigits)
+        {
+            StringBuilder sb = new();
+            sb.Append(screen.text).Append(number);
+            screen.text = sb.ToString();
+        }
+    }
+
+    public void KeypadDeleteInput()
+    {
+        if (screen.text != null && screen.text != "")
+        {
+            screen.text.Remove(screen.text.Length - 1);
+        }
+    }
+
+    public void KeypadEnterInput()
+    {
+        StringBuilder sb = new();
+        sb.Append("FUCK");
+        if (screen.text.Equals(combination.ToString()))
+        {
+            sb.Append(" YEAH");
+        }
+        sb.Append("!!!");
+        Debug.Log(sb.ToString());
+    }
+}
