c7df134d1b8b24c8c3d706740dfb45dea12a68df
MVBNV700 <vikstrom18noah@gmail.com>
2022-10-26 12:51:17 +0200

    Started on PauseMenu in NoahScene

--- Assets/UI 1/Scripts/MenuBehaviour.cs
+++ Assets/UI 1/Scripts/MenuBehaviour.cs
@@ -5,6 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class MenuBehaviour : MonoBehaviour
 {
+
+    [SerializeField] private bool isPauseMenu = false;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape) && isPauseMenu)
+        {
+            PauseGame();
+        }
+    }
+
     // Method to load a scene by insert the index of wished scene presented in buildsettings
     public void SelectScene(int sceneIndex)
     {
@@ -18,7 +29,15 @@ public class MenuBehaviour : MonoBehaviour
         }
     }
 
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+    }
 
+    public void UnPauseGame()
+    {
+        Time.timeScale = 1f;
+    }
 
     // Method to quit the application anytime
     public void QuitGame()
