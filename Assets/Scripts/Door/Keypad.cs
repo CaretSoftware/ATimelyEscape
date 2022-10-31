81bb9161445c49e3de334b80ee2b80ecd2e54c25
MVBNV700 <vikstrom18noah@gmail.com>
2022-10-31 18:18:22 +0100

    Updated KeyPad-Script
    Jag har lagt ill 4 rader som ändrar knappens utseende beroende på dess state

--- Assets/Scripts/Door/Keypad.cs
+++ Assets/Scripts/Door/Keypad.cs
@@ -94,12 +94,16 @@ public class Keypad : DeviceController
         }
         if (Input.GetKeyDown(KeyCode.Delete) || Input.GetKeyDown(KeyCode.Backspace))
         {
+            // To change buttons Appearence to Pressed
+            keypadKeys[currentKeypadKeysIndex].gameObject.GetComponent<ButtonBehaviour>().ToPressedSprite();
             KeypadDeleteInput();
         }
         else if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
         {
             if (usingKeyboardDigits)
             {
+                // To change buttons Appearence to Pressed
+                keypadKeys[currentKeypadKeysIndex].gameObject.GetComponent<ButtonBehaviour>().ToPressedSprite();
                 KeypadEnterInput();
             }
         }
@@ -124,6 +128,9 @@ public class Keypad : DeviceController
         }
         if (currentKeypadKeysIndex != previousKeypadIndex)
         {
+            // To change previousbuttons Appearence to Neutral
+            keypadKeys[previousKeypadIndex].gameObject.GetComponent<ButtonBehaviour>().ToNeutralSprite();
+
             if (usingKeyboardDigits)
             {
                 currentKeypadKeysIndex = previousKeypadIndex;
@@ -133,13 +140,15 @@ public class Keypad : DeviceController
                 previousKeypadIndex = currentKeypadKeysIndex;
             }
             keypadKeys[currentKeypadKeysIndex].Select();
+
+            // To change buttons Appearence to Selected
+            keypadKeys[currentKeypadKeysIndex].gameObject.GetComponent<ButtonBehaviour>().ToSelectedSprite();
+
             usingKeyboardDigits = false;
         }
         if (usingKeyboardDigits)
         {
             EventSystem.current.SetSelectedGameObject(null);
         }
-        
-
     }
 }
