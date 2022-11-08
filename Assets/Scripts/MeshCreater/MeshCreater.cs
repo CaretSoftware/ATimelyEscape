f8abc10fbefb5e318ebb0eb763235699386c6ef0
Ossian Däckfors <ossian.dackfors@gmail.com>
2022-11-08 10:21:05 +0100

    h

--- Assets/Scripts/MeshCreater/MeshCreater.cs
+++ Assets/Scripts/MeshCreater/MeshCreater.cs
@@ -1,31 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static UnityEngine.Mesh;
 
 [RequireComponent(typeof(MeshFilter))]
+//[RequireComponent(typeof(MeshRenderer))]
 [RequireComponent(typeof(MeshCollider))]
 public class MeshCreater : MonoBehaviour
 {
+    [SerializeField] private bool removeVertexObjects;
     [SerializeField] private MeshFilter meshFilter;
     [SerializeField] private MeshCollider meshCollider;
     [SerializeField] private Mesh mesh;
 
-    [SerializeField] private Transform[] vertices;
+    //[SerializeField] private Transform[] vertices;
 
-    private Vector3[] verticesPositions;
+    private Vector3[] vertices;
+    //private Vector2[] uvs;
 
     private void Awake()
     {
         AssignComponents();
 
-        verticesPositions = new Vector3[vertices.Length];
-        for (int i = 0; i < vertices.Length; i++)
-        {
-            verticesPositions[i] = vertices[i].position;
-        }
+        vertices = new Vector3[transform.childCount];
+        //uvs = new Vector2[vertices.Length];
+        SetVertices();
+        //SetUV();
 
         RenderMesh();
+        if (removeVertexObjects)
+        {
+            foreach (Transform child in transform)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+    }
+
+    private void SetVertices()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            vertices[i] = transform.GetChild(i).localPosition;
+        }
     }
 
     public void AssignComponents()
@@ -38,16 +56,36 @@ public class MeshCreater : MonoBehaviour
     private void RenderMesh()
     {
         mesh.Clear();
-
-        mesh.vertices = verticesPositions;
-
-        //mesh.SetTriangles(meshData.triangles.ToArray(), 0);
-
-        //mesh.uv = meshData.uv.ToArray();
-        mesh.RecalculateNormals();
         meshCollider.sharedMesh = null;
+        mesh.vertices = vertices;
+        //mesh.uv = uvs;
+        mesh.SetTriangles(AddTriangles(), 0);
+        
+
+        mesh.RecalculateNormals();
+        
         meshCollider.sharedMesh = mesh;
 
+    }
+
+    //private void SetUV()
+    //{
+    //    for (int i = 0; i < uvs.Length; i++)
+    //    {
+    //        uvs[i] = new Vector2(vertices[i].x, vertices[i].z);
+    //    }
+    //}
+
+    private int[] AddTriangles()
+    {
+        int[] triangels = new int[vertices.Length * 3];
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            triangels[i + (2 * i)] = i;
+            triangels[(i + 1) + (2 * i)] = (i + 2) % vertices.Length;
+            triangels[(i + 2) + (2 * i)] = (i + 2) % vertices.Length;
+        }
+        return triangels;
     }
 
 }
