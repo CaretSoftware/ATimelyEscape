d740e47e1375fb3a49fafd844292551ab0c243cf
Ossian Däckfors <ossian.dackfors@gmail.com>
2022-11-09 14:21:32 +0100

    Update LightDetection.cs

--- Assets/Scripts/Darkness/LightDetection.cs
+++ Assets/Scripts/Darkness/LightDetection.cs
@@ -92,7 +92,7 @@ namespace CallbackSystem
 
                 }
                 vignette.intensity.value = timer;
-                Debug.Log(vignette.intensity.value);
+                //Debug.Log(vignette.intensity.value);
                 
             }
         }
