2bae048d2fe021fbf1629a423869f64dd2d369c8
Johan Salqvist <josa8681@student.su.se>
2022-11-10 14:55:23 +0100

    shader

---
+++ Assets/Johan & Emil PROD/Outline.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Outline : MonoBehaviour
+{
+    public MeshRenderer outlineRenderer;
+    public GameObject gameObjectTest;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObjectTest = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
