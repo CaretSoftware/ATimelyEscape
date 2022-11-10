2a5259cf76dbcd6c8e73b418225d043231e59e32
Leo Lindblom <leolindblom1@gmail.com>
2022-11-10 17:47:37 +0100

    CrossHairScript

---
+++ Assets/Scripts/CrosshairScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrosshairScript : MonoBehaviour
+{
+    [SerializeField] private Image crosshair;
+    [SerializeField] private LayerMask layerMask;
+    private void Start()
+    {
+        crosshair.color = new Color(1, 1, 1, 0.75f);
+    }
+
+    private void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 50f, layerMask))
+        {
+           // if (hit.transform.gameObject.CompareTag("Cube"))
+            {
+                crosshair.color = new Color(1, 0, 0, 0.75f);
+            }
+
+        }
+        else
+        {
+            crosshair.color = new Color(1, 1, 1, 0.75f);
+        }
+    }
+}
