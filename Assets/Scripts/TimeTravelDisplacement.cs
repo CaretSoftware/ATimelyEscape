5cb4c09cff820b1c5c63be8d7c0c91d779e81e81
CaretSoftware <pbergsten@me.com>
2022-11-20 00:04:47 +0100

    Cube Time Travel Feedback

--- Assets/Scripts/TimeTravelDisplacement.cs
+++ Assets/Scripts/TimeTravelDisplacement.cs
@@ -26,33 +26,39 @@ public class TimeTravelDisplacement : MonoBehaviour {
 		mr = GetComponent<MeshRenderer>();
 	}
 
-	private void Update() {
-		t = Mathf.Sin(Time.time * Mathf.PI) * .5f + .5f;
-		Displace(t);
-		// if (Input.GetKeyDown(KeyCode.Q))
-		// 	MoveAndRotateMesh();
-	}
+	// private void Update() {
+	// 	t = Mathf.Sin(Time.time * Mathf.PI) * .5f + .5f;
+	// 	Displace(t);
+	// 	// if (Input.GetKeyDown(KeyCode.Q))
+	// 	// 	MoveAndRotateMesh();
+	// }
 
-	public void MoveAndRotateMesh() {
+	private void MoveAndRotateMesh() {
 		if (other == null || mr == null) return;
 		StopAllCoroutines();
 		StartCoroutine(Displace());
 	}
 
+	public void Displace(Transform target) {
+		other = target;
+		Debug.LogWarning(target.name);
+		MoveAndRotateMesh();
+	}
+	
 	private IEnumerator Displace() {
 		float t = 0.0f;
 		float velocity = 1.0f / time;
 		
-		OutlineStrength(0f);
+		// OutlineStrength(0f);
 		
 		while (t < 1.0f) {
 			t += velocity * Time.deltaTime;
 			Displace(Ease.EaseInOutCubic(t));
-			OutlineStrength(1.0f - t);
+			// OutlineStrength(1.0f - t);
 			yield return null;
 		}
 		Displace(0.0f);
-		StartCoroutine(FadeInOutline());
+		//StartCoroutine(FadeInOutline());
 	}
 	
 	private void Displace(float t) {
