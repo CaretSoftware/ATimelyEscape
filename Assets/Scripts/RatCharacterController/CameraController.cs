3306554e26e8201cdf2ab3652457e4f06cdf2f5c
CaretSoftware <pbergsten@me.com>
2022-11-25 03:25:44 +0100

    Update CameraController.cs

--- Assets/Scripts/RatCharacterController/CameraController.cs
+++ Assets/Scripts/RatCharacterController/CameraController.cs
@@ -2,6 +2,7 @@ using System;
 using Unity.Mathematics;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class CameraController : MonoBehaviour {
 	public static CameraController Instance { get; private set; }
@@ -11,39 +12,39 @@ public class CameraController : MonoBehaviour {
 		set => mouseSensitivity = Mathf.Max(0.0f, value);
 	}
 
-	[SerializeField] private Transform _camera;
-	private float mouseSensitivity = .7f;
+	[SerializeField] 
+	private Transform _camera;
+	private Transform _rat;
 	
+	private const float ClampLookupMax = 50.0f;
+	private const float ClampLookupMin = 175.0f;
+	private float mouseSensitivity = .7f;
 	private Vector2 _mouseMovement;
 	private Vector3 _cameraPos;
 	
-	private float _clampLookupMax = 50.0f;
-	private float _clampLookupMin = 175.0f;
-	public float smoothDampMinVal;
-	public float smoothDampMaxVal;
 
-	private Vector3 _smoothDampCurrentVelocityLateral;
+	private Vector3 _velocityLateral;
 	private Vector3 _smoothDampCurrentVelocity;
-		
+
 	
 	[Header("Camera Settings")]
-	[SerializeField, Range(0.0f, 2.0f)]
-	private float _headHeight = .16f;
-	[SerializeField]
-	private bool firstPerson;
 	[SerializeField] 
 	private LayerMask collisionMask;
 	[SerializeField] 
 	private Vector3 cameraOffset;
 	[SerializeField] [Range(0.0f, 1.0f)] 
 	private float smoothness = 0.05f;
+	[SerializeField] private float smoothDampMinVal;
+	[SerializeField] private float smoothDampMaxVal;
+	
 	private float _cameraCollisionRadius = .029f;
+	private readonly Vector3 _headHeight = new Vector3(0, .16f, 0);
 
 	private Vector2 _thumbstickDelta;
 
 	private PauseMenuBehaviour pauseMenuBehaviour;
 
-	private Transform dragTransform;
+	private Vector3 dragTransform;
 	
 	private void Awake() {
 		// if (Instance == null) 
@@ -62,13 +63,14 @@ public class CameraController : MonoBehaviour {
 		Vector3 initialCameraVector = cameraFollow.Follow.rotation.eulerAngles;
 		_mouseMovement = new Vector2(initialCameraVector.y, initialCameraVector.x);
 		
-		_lerpOffset = cameraOffset;
+		_offset = cameraOffset;
 		_cameraPos = transform.position;
 		_camera.position = _cameraPos + cameraOffset;
 
-		dragTransform = new GameObject("DragTransform").transform;
-		dragTransform.position = _camera.position;
-		// dragTransform = Instantiate(new GameObject("Drag Transform"), _camera.position, _camera.rotation).transform;
+		dragTransform = _camera.position;
+
+		_rat = FindObjectOfType<RatCharacterController.CharacterInput>().transform;
+		_ratRigidBody = _rat.GetComponent<Rigidbody>();
 
 		Cursor.visible = false;
 		Cursor.lockState = CursorLockMode.Locked;
@@ -77,9 +79,9 @@ public class CameraController : MonoBehaviour {
 	private void LateUpdate() {
 		
 		if (pauseMenuBehaviour != null && pauseMenuBehaviour.isPaused()) return;
-		
+
+		ClampCameraTilt();
 		DragCameraBehind();
-		// MoveCamera();
 	}
 
 	
@@ -87,38 +89,82 @@ public class CameraController : MonoBehaviour {
 		const float mouseSensitivity = 25f;
 		_mouseMovement.x += mouseDelta.x * MouseSensitivity * Time.deltaTime * mouseSensitivity;
 		_mouseMovement.y -= mouseDelta.y * MouseSensitivity * Time.deltaTime * mouseSensitivity;
-		
-		// TODO testing to see if we can use mouseDelta instead
-		_mouseDelta.x = mouseDelta.x * MouseSensitivity * Time.deltaTime * mouseSensitivity;
-		_mouseDelta.y = mouseDelta.y * MouseSensitivity * Time.deltaTime * mouseSensitivity;
 	}
 
 	public void StickInput(Vector2 stickDelta) {
 		const float stickSensitivity = 150f;
 		
 		_thumbstickDelta = stickSensitivity * Time.deltaTime * new Vector2(stickDelta.x, -stickDelta.y);
+		_mouseMovement += _thumbstickDelta;
 	}
 
-	private Vector3 _lerpOffset;
-	private Vector2 prevInput;
-
-	private void MoveCamera() {
+	private void ClampCameraTilt() {
 		const float lookOffset = 90;
-
-		_mouseMovement += _thumbstickDelta;
+		const float min = ClampLookupMax - lookOffset;
+		const float max = ClampLookupMin - lookOffset;
 		
-		_mouseMovement.y = Mathf.Clamp(_mouseMovement.y, _clampLookupMax - lookOffset, _clampLookupMin - lookOffset);
+		_mouseMovement.y = Mathf.Clamp(_mouseMovement.y, min, max);
+	}
+
+	private Rigidbody _ratRigidBody;
+	private float autoRotationSpeed = 300f;
+	private Vector3 _ratLeft;
+	private Vector3 _ratDampVelocity;
+	private Vector3 _offset;
+	private float _lastMouseMovementX;
+	private void DragCameraBehind() {
+		_ratLeft = 
+				Vector3.SmoothDamp(
+					_ratLeft, 
+					-_rat.right, 
+					ref _ratDampVelocity,
+					.5f, 
+					float.MaxValue);
+		
+		if (NoMouseMovement()) {
+			float dot = Vector3.Dot(_camera.forward, _ratLeft);
+			Vector3 velocity = _ratRigidBody.velocity;
+			velocity.y = 0;
+			_mouseMovement.x += dot * velocity.magnitude * autoRotationSpeed * Time.deltaTime;
+		}
 
 		_camera.rotation = Quaternion.Euler(_mouseMovement.y, _mouseMovement.x, 0.0f);
-	
-		if (firstPerson) {
-			_camera.localPosition = transform.position + _headHeight * Vector3.up;
-			return;
+
+		Vector3 abovePlayer = transform.position + _headHeight;
+		
+		_offset = CalculateOffsetDistance();
+		
+		_camera.position = abovePlayer + _camera.rotation * _offset;
+
+		_lastMouseMovementX = _mouseMovement.x;
+		bool NoMouseMovement() {
+			return Mathf.Abs(_lastMouseMovementX - _mouseMovement.x) <= float.Epsilon;
 		}
+		
+		Vector3 CalculateOffsetDistance() {
+			Vector3 offsetTarget = abovePlayer + _camera.rotation * cameraOffset;
+			Vector3 offsetDirection = offsetTarget - abovePlayer;
+			
+			Physics.SphereCast(abovePlayer, 
+				_cameraCollisionRadius, 
+				offsetDirection.normalized, 
+				out RaycastHit hit, 
+				offsetDirection.magnitude, 
+				collisionMask);
+		
+			Vector3 offset = hit.collider ? cameraOffset.normalized * hit.distance : cameraOffset;
 	
+			float smoothDollyTime = hit.collider ? smoothDampMinVal : smoothDampMaxVal;
+			return Vector3.SmoothDamp(_offset, offset, ref _smoothDampCurrentVelocity, smoothDollyTime);
+		}
+	}
+
+	private void MoveCamera() {
+		_camera.rotation = Quaternion.Euler(_mouseMovement.y, _mouseMovement.x, 0.0f);
+
 		_cameraPos = Vector3.SmoothDamp(_cameraPos, transform.position, 
-				ref _smoothDampCurrentVelocityLateral, smoothness);
-		Vector3 abovePlayer = _cameraPos + Vector3.up * _headHeight;
+				ref _velocityLateral, smoothness);
+		Vector3 abovePlayer = _cameraPos + _headHeight;
 		Vector3 offsetTarget = abovePlayer + _camera.rotation * cameraOffset;
 		Vector3 offsetDirection = offsetTarget - abovePlayer;
 		
@@ -129,63 +175,11 @@ public class CameraController : MonoBehaviour {
 			offsetDirection.magnitude, 
 			collisionMask);
 	
-		Vector3 offset;
-		if (hit.collider)
-			offset = cameraOffset.normalized * hit.distance;
-		else
-			offset = cameraOffset;
+		Vector3 offset = hit.collider ? cameraOffset.normalized * hit.distance : cameraOffset;
 
 		float smoothDollyTime = hit.collider ? smoothDampMinVal : smoothDampMaxVal;
-		_lerpOffset = Vector3.SmoothDamp(_lerpOffset, offset, ref _smoothDampCurrentVelocity, smoothDollyTime);
+		_offset = Vector3.SmoothDamp(_offset, offset, ref _smoothDampCurrentVelocity, smoothDollyTime);
 		
-		_camera.position = abovePlayer + _camera.rotation * _lerpOffset;
-	}
-
-	[SerializeField] private Transform _followTransform;
-	private Vector3 _cameraDirection;
-	private Vector3 _cameraHeight;
-	private Vector2 _mouseDelta;
-	private void DragCameraBehind() {
-		float distance = .3f;
-		Vector3 headHeight = Vector3.up * _headHeight;
-
-		DragCameraPosition();
-		
-		void DragCameraPosition() {
-			
-			_cameraDirection = (dragTransform.position - _followTransform.position);
-			_cameraDirection = _cameraDirection.ProjectOnPlane();
-			_cameraDirection.Normalize();
-		}
-
-		RotateCameraPosition();
-		
-		void RotateCameraPosition() {
-			const float lookOffset = 90;
-
-			_cameraDirection = Quaternion.Euler(0, _mouseDelta.x, 0.0f) * _cameraDirection;
-			// _cameraHeight = Quaternion.Euler(_mouseDelta.y, 0.0f, 0.0f) * _cameraHeight;
-			// Debug.Log(_cameraHeight);
-			// Quaternion.Euler(_mouseDelta.y, 0.0f, 0.0f).eulerAngles;
-			//
-			// _cameraHeight.y = Mathf.Clamp(_cameraHeight.y, _clampLookupMax - lookOffset, _clampLookupMin - lookOffset);
-
-			// Reset mouse delta
-			_mouseDelta = Vector2.zero;
-		}
-
-		dragTransform.position = _followTransform.position + headHeight + _cameraDirection * distance;
-		
-		
-		_camera.position = dragTransform.position;
-		_camera.LookAt(_followTransform);
-
-
-		// Vector3 initialCameraVector = (_camera.position - transform.position).normalized;
-		// cameraFollow.Follow.rotation.eulerAngles;
-
-		// _mouseMovement = new Vector2(cameraDirection.y, cameraDirection.x);
-
+		_camera.position = abovePlayer + _camera.rotation * _offset;
 	}
 }
-	
