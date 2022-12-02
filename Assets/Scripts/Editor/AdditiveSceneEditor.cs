158f4132af189eddb96d3939cda0a167d7ec9ad6
CaretSoftware <pbergsten@me.com>
2022-12-02 02:51:41 +0100

    Additive Scene Manager
    code cleanup

--- Assets/Scripts/Editor/AdditiveSceneEditor.cs
+++ Assets/Scripts/Editor/AdditiveSceneEditor.cs
@@ -3,41 +3,43 @@ using UnityEditor;
 
 [CustomEditor(typeof(AdditiveSceneManager))]
 public class AdditiveSceneEditor : Editor {
+	
+	private AdditiveSceneManager _ase;
+	private GUIContent _save;
+	private GUIContent _visibilityMixed;
+	private GUIContent _hidden;
+	private GUIContent[] _loads;
+	
 	public override void OnInspectorGUI() {
-		AdditiveSceneManager ase = (AdditiveSceneManager) target;
+		_ase ??= (AdditiveSceneManager)target;
 		
-		GUIContent save =
+		_save ??=
 				new GUIContent(" Save",
 						EditorGUIUtility.IconContent("d_SaveAs").image,
 				"Save All open additive scenes");
-		GUIContent visibilityMixed = 
+		_visibilityMixed ??= 
 				new GUIContent(" Load", 
 						EditorGUIUtility.IconContent("d_scenevis_visible-mixed_hover@2x").image, 
 						"Load and Open all unopened scenes");
-		GUIContent hidden = 
+		_hidden ??= 
 				new GUIContent(" Load Unloaded", 
 						EditorGUIUtility.IconContent("d_scenevis_hidden_hover@2x").image, 
 						"Load all unopened scenes unloaded");
 
-		GUIContent[] loads = new GUIContent[] { visibilityMixed, hidden };
+		_loads ??= new GUIContent[] { _visibilityMixed, _hidden };
 		
-		if (GUILayout.Button(save))
-			ase.SaveScenes();
+		if (GUILayout.Button(_save))
+			_ase.SaveScenes();
 
 		GUILayout.BeginHorizontal();
 
 		int selected = -1;
-		selected = GUILayout.SelectionGrid(selected, loads, 2);
-			
-		// if (GUILayout.Button(visibilityMixed))
-		// 	ase.LoadScenes();
-		//
-		// if (GUILayout.Button(hidden))
-		// 	ase.LoadScenes(false);
+		selected = GUILayout.SelectionGrid(selected, _loads, 2);
+		
 		if (selected == 0)
-			ase.LoadScenes();
+			_ase.LoadScenes();
 		if (selected == 1)
-			ase.LoadScenes(false);
+			_ase.LoadScenes(false);
 
 		GUILayout.EndHorizontal();
 	}
