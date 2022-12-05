2f3471bdc3ccd906519508ea193d2fab9078c131
CaretSoftware <69549081+CaretSoftware@users.noreply.github.com>
2022-12-05 23:00:29 +0100

    Update RuntimeSceneManager.cs

--- Assets/Scripts/RuntimeSceneManager.cs
+++ Assets/Scripts/RuntimeSceneManager.cs
@@ -1,32 +1,72 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class RuntimeSceneManager : MonoBehaviour
-{
-    private static int[] _room0 = new[] { 0, 1, 2 };
-    private static int[] _room1 = new[] { 1, 2, 3 };
-    private static int[] _room2 = new[] { 2, 3, 4 };
-    private static int[] _room3 = new[] { 3, 4, 5 };
-    private static int[] _room4 = new[] { 4, 5, 6 };
-    private static int[] _room5 = new[] { 5, 6, 7 };
-    private static int[] _room6 = new[] { 6, 7 };
-    private static int[] _room7 = new[] { 7 };
+public class RuntimeSceneManager : MonoBehaviour {
+    private static readonly HashSet<int> Room0 = new HashSet<int>() { 0, 1, 2 };    // cage room
+    private static readonly HashSet<int> Room1 = new HashSet<int> { 1, 2, 3 };      // Incubator
+    private static readonly HashSet<int> Room2 = new HashSet<int> { 2, 3, 4 };      // Office
+    private static readonly HashSet<int> Room3 = new HashSet<int> { 4, 5, 6 };      // Corridor
+    private static readonly HashSet<int> Room4 = new HashSet<int> { 5, 6, 7 };      // Lab Large
+    private static readonly HashSet<int> Room5 = new HashSet<int> { 6, 7, 8 };      // Control Room
+    private static readonly HashSet<int> Room6 = new HashSet<int> { 7, 8, 9 };      // Conveyor room
+    private static readonly HashSet<int> Room7 = new HashSet<int> { 8, 9, 10 };     // Robot Factory
+    private static readonly HashSet<int> Room8 = new HashSet<int> { 9, 10, 11 };    // Garden
+    private static readonly HashSet<int> Room9 = new HashSet<int> { 10, 11, 12 };   // Cryo Hall
+    private static readonly HashSet<int> Room10 = new HashSet<int> { 11, 12, 13 };  // Cryo Room
 
-    private static int[][] _rooms = new int[][] {
-        _room0,    // 0 cageRoom 
-        new int[] { 1, 2, 3 },    // 1 Incubator
-        new int[] { 2, 3, 4 },    // 3 
-        new int[] { 3, 4, 5 },    // 4 
-        new int[] { 4, 5, 6 },    // 5 
-        new int[] { 5, 6, 7 },    // 6 
-        new int[] { 6, 7 },    // 7 
+    private static readonly HashSet<int>[] Rooms = new HashSet<int>[] {
+        Room0, 
+        Room1,
+        Room2,
+        Room3,
+        Room4,
+        Room5,
+        Room6,
+        Room7,
+        Room8,
+        Room9,
+        Room10
     };
     
     private int _currentRoom = 0;
     
     private void Awake() {
         
+    }
+
+    public void RatEnteredRoom(int room) {
+        LoadNeighbouringRooms(room);
+    }
+
+    private void LoadNeighbouringRooms(int room) {
+        if (room == _currentRoom) return;
+
+        HashSet<int> exceptionScenes = new HashSet<int>(Rooms[room]);
+        exceptionScenes.ExceptWith(Rooms[_currentRoom]);
+        exceptionScenes.UnionWith(Rooms[_currentRoom]);
+        int[] scenesToUnload = exceptionScenes.ToArray();
+        
+        // unload rooms
+        for (int sceneToUnload = 0; sceneToUnload < scenesToUnload.Length; sceneToUnload++) {
+            // for (int previousRooms = 0; previousRooms < UPPER; previousRooms++)
+            // {
+                
+            // }
+            SceneManager.UnloadSceneAsync(0, UnloadSceneOptions.None);
+        }
+        
+        _currentRoom = room;
+        
+        // load rooms
+        for (int neighbourRooms = 0; neighbourRooms < Rooms[room].Count; neighbourRooms++) {
+           // int sceneToLoad = Rooms[room][neighbourRooms];
+           // if (sceneToLoad == _currentRoom) continue;
+            
+           // SceneManager.LoadSceneAsync(sceneToLoad, LoadSceneMode.Additive);
+        }
     }
 }
