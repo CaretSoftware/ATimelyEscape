989a90fa367cbe341993c059f4f55dada8db0312
Ossian Däckfors <ossian.dackfors@gmail.com>
2022-12-06 16:07:55 +0100

    d

--- Assets/Scripts/CubeCharge.cs
+++ Assets/Scripts/CubeCharge.cs
@@ -5,7 +5,8 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 
-namespace CallbackSystem {
+namespace CallbackSystem
+{
     [RequireComponent(typeof(CubePush))]
     [RequireComponent(typeof(TimeTravelObject))]
     [RequireComponent(typeof(MeshRenderer))]
@@ -15,16 +16,15 @@ namespace CallbackSystem {
 
         [Tooltip("Charge reducese with 1 every forward jump in time. At 0 cube can no longer be puched")]
         public int charge = 0;
-        [HideInInspector]public CubeCharge pastCubeCharge;
+        [SerializeField] private float timeToCharge;
+        [HideInInspector] public CubeCharge pastCubeCharge;
 
-        [Header("Materials")]
-        [SerializeField] private Material onMaterial;
-        [SerializeField] private Material offMaterial;
 
         private CubePush cubePush;
         private ChargeChangedEvent chargeEvent;
         private TimeTravelObject timeTravelObject;
         private MeshRenderer meshRenderer;
+        private PingPong pingPong;
 
         private IconBehaviour iconBehaviour;
 
@@ -35,24 +35,27 @@ namespace CallbackSystem {
             cubePush = GetComponent<CubePush>();
             timeTravelObject = GetComponent<TimeTravelObject>();
             meshRenderer = GetComponent<MeshRenderer>();
+            pingPong = GetComponent<PingPong>();
             if (charge > 0)
             {
                 cubePush.SetPushable(true);
                 iconBehaviour.IsCharged(true);
 
-            } else
+            }
+            else
             {
                 cubePush.SetPushable(false);
                 iconBehaviour.IsCharged(false);
             }
-                
+
             chargeEvent = new(timeTravelObject);
 
-            if(timeTravelObject.pastSelf != null && timeTravelObject.pastSelf.gameObject.GetComponent<CubeCharge>() != null)
+            if (timeTravelObject.pastSelf != null && timeTravelObject.pastSelf.gameObject.GetComponent<CubeCharge>() != null)
             {
                 pastCubeCharge = timeTravelObject.pastSelf.gameObject.GetComponent<CubeCharge>();
             }
             ChargeChangedEvent.AddListener<ChargeChangedEvent>(PastChargeChange);
+            TimePeriodChanged.AddListener<TimePeriodChanged>(changedTime);
         }
 
         public void Charging(int charge, Object origin)
@@ -64,11 +67,11 @@ namespace CallbackSystem {
                 {
                     cubePush.SetPushable(true);
                     iconBehaviour.IsCharged(true);
-                    SetMaterial(onMaterial);
+                    StartCoroutine(SetMaterial(true));
                 }
                 else
                 {
-                    SetMaterial(offMaterial);
+                    StartCoroutine(SetMaterial(false));
                     iconBehaviour.IsCharged(false);
                 }
                 if (!origin.GetType().Equals(GetType()))
@@ -80,7 +83,7 @@ namespace CallbackSystem {
 
         private void PastChargeChange(ChargeChangedEvent chargeChangedEvent)
         {
-            if (timeTravelObject.pastSelf.pastSelf != null & timeTravelObject.pastSelf.pastSelf.Equals(chargeChangedEvent.changedObject)) 
+            if (timeTravelObject.pastSelf.pastSelf != null & timeTravelObject.pastSelf.pastSelf.Equals(chargeChangedEvent.changedObject))
             {
                 Charging(pastCubeCharge.pastCubeCharge.charge - (2 * chargeReductionAfterTimeJump), this);
             }
@@ -90,9 +93,35 @@ namespace CallbackSystem {
             }
         }
 
-        private void SetMaterial(Material material)
+        private IEnumerator SetMaterial(bool on)
         {
-            meshRenderer.material = material;
+            float timer = 0f;
+            while (timer < 0)
+            {
+                if (on)
+                {
+                    timer += Time.deltaTime / timeToCharge;
+                    pingPong.SetPower(Mathf.Lerp(0, 1, timer));
+                    yield return null;
+                }
+                else
+                {
+                    timer += Time.deltaTime / timeToCharge;
+                    pingPong.SetPower(Mathf.Lerp(1, 0, timer));
+                    yield return null;
+                }
+            }
         }
-    } 
+        private void changedTime(TimePeriodChanged timePeriodChanged)
+        {
+            if (charge > 0)
+            {
+                pingPong.SetPower(1);
+            }
+            else
+            {
+                pingPong.SetPower(0);
+            }
+        }
+    }
 }
