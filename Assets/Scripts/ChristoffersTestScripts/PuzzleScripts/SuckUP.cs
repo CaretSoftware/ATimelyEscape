fa0aec309b94f54524e965a6bb47c242a197633f
Boppan <38586588+Boppan@users.noreply.github.com>
2022-12-13 09:47:29 +0100

    part system vacuum

--- Assets/Scripts/ChristoffersTestScripts/PuzzleScripts/SuckUP.cs
+++ Assets/Scripts/ChristoffersTestScripts/PuzzleScripts/SuckUP.cs
@@ -6,11 +6,59 @@ public class SuckUP : MonoBehaviour
 {
     [SerializeField] private Transform suckPosition;
     [SerializeField] private float suckSpeed;
-    private void OnTriggerEnter(Collider other)
+    private bool buttonOneOn;
+    private bool buttonTwoOn;
+    private float spinX;
+    private float spinY;
+    private float spinZ;
+    private Vector3 suckDirection;
+    private Animator animator;
+    private ParticleSystem ps;
+    private void Start()
     {
-        if(other.gameObject.tag == "Enemy")
+        animator = GetComponentInParent<Animator>();
+        ps = GetComponentInChildren<ParticleSystem>();
+    }
+    private void Update()
+    {
+        if (buttonOneOn && buttonTwoOn)
         {
-            other.gameObject.transform.position = suckPosition.position;
+            animator.SetBool("Start", true);
+            StartCoroutine(Suck());
         }
     }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+            suckDirection = suckPosition.position - other.gameObject.transform.position;
+            spinX = Random.Range(50, 100) * Time.deltaTime;
+            spinY = Random.Range(20, 150) * Time.deltaTime;
+            spinZ = Random.Range(50, 300) * Time.deltaTime;
+            other.gameObject.transform.Rotate(spinX, spinY, spinZ, Space.Self);
+            rb.AddForce((suckDirection * suckSpeed) * Time.deltaTime);
+        }
+    }
+
+    public void ButtonOneOn()
+    {
+        buttonOneOn = true;
+    }
+    public void ButtonTwoOn()
+    {
+        buttonTwoOn = true;
+    }
+    private IEnumerator Suck()
+    {
+        ps.Play();
+        Debug.Log("suck");
+        yield return new WaitForSeconds(2.0f);
+        Debug.Log("stop");
+        ps.Stop();
+        animator.SetBool("Stop", true);
+    }
+
+
+
 }
