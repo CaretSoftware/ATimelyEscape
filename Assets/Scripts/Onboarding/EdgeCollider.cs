e051db637034ee1617282e9a4cf45fc02e8cac6c
gretgurt <gretahassler@hotmail.com>
2022-12-17 20:06:42 +0100

    Added collider that resets player position when too far from task

---
+++ Assets/Scripts/Onboarding/EdgeCollider.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdgeCollider : MonoBehaviour
+{
+    private Vector3 spawnPosition;
+
+    void Start()
+    {
+        spawnPosition = FindObjectOfType<RatCharacterController.CharacterInput>().transform.position;
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.transform.position = spawnPosition;
+    }
+}
