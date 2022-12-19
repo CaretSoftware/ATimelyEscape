42a3942f3c5b67d81b1ddd0f592ca445e685d109
Boppan <38586588+Boppan@users.noreply.github.com>
2022-12-19 22:16:17 +0100

    Room  6

--- Assets/Scripts/ChristoffersTestScripts/PuzzleScripts/ActivateBridge.cs
+++ Assets/Scripts/ChristoffersTestScripts/PuzzleScripts/ActivateBridge.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class ActivateBridge : MonoBehaviour
 {
     [SerializeField] private GameObject bridge;
+    [SerializeField] private GameObject bridge2;
 
     private void Awake()
     {
         bridge.SetActive(false);
+        if(bridge2 != null)
+        {
+            bridge2.SetActive(false);
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Cube")
         {
             bridge.SetActive(true);
+            if (bridge2 != null)
+            {
+                bridge2.SetActive(true);
+            }
         }
     }
     private void OnTriggerExit(Collider other)
@@ -22,6 +31,10 @@ public class ActivateBridge : MonoBehaviour
         if (other.gameObject.tag == "Cube")
         {
             bridge.SetActive(false);
+            if (bridge2 != null)
+            {
+                bridge2.SetActive(false);
+            }
         }
     }
 }
