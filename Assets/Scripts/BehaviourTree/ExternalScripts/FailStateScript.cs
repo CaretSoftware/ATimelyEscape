7fe22d9a61f634df162e767498a099fb7685c0d8
August <aggeh@hotmail.se>
2022-12-20 16:04:27 +0100

    tag check

--- Assets/Scripts/BehaviourTree/ExternalScripts/FailStateScript.cs
+++ Assets/Scripts/BehaviourTree/ExternalScripts/FailStateScript.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using CallbackSystem;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.UI;
 using Event = CallbackSystem.Event;
 
@@ -30,15 +32,21 @@ public class FailStateScript : MonoBehaviour
 
     private void Start()
     {
-        player = GameObject.Find("Player/Rat Mesh").transform;
+        //player = GameObject.FindObjectOfType<NewCharacterController>().transform;
         hyperDriveAnimator = GetComponent<Animator>();
         imageFunctionality = GameObject.Find("FailStateCanvas/BlackScreen").GetComponent<ImageFadeFunctions>();
     }
 
     //Call on when player dies.
-    public void PlayDeathVisualization(Transform checkpoint)
+    public void PlayDeathVisualization(Transform checkpoint, Transform goTransform = null)
     {
-        print($"animations triggered");
+        if(goTransform.tag.Equals("Scientist")){}
+            //Play Scientist Event
+        else if(goTransform.tag.Equals("Roomba")){}
+            //Play Roomba Event
+            
+        // catch the rat
+        //NewCharacterController.caughtEvent?.Invoke(true);
         this.checkpoint = checkpoint;
         hyperDriveAnimator.gameObject.SetActive(true);
         hyperDriveAnimator.SetTrigger("DeathAnimTrigger");
@@ -50,6 +58,8 @@ public class FailStateScript : MonoBehaviour
     public void FadeBack()
     {
         player.position = checkpoint.position;
+        // release the rat
+        //NewCharacterController.caughtEvent?.Invoke(false);
         imageFunctionality.RunFadeBack();
         hyperDriveAnimator.gameObject.SetActive(false);
     }
