6d132eed66c50c5ac07132cd811b6fd8bcd6a82b
CaretSoftware <69549081+CaretSoftware@users.noreply.github.com>
2023-01-10 23:29:27 +0100

    Fix TT LockState

--- Assets/Scripts/RatCharacterController/RatCharacterController/StateMachine/LockState.cs
+++ Assets/Scripts/RatCharacterController/RatCharacterController/StateMachine/LockState.cs
@@ -4,9 +4,11 @@ namespace NewRatCharacterController
 {
     public class LockState : BaseState {
         private const string State = nameof(LockState);
-
+        private bool _couldTimeTravel;
+        
         public override void Enter() {
             StateChange.stateUpdate?.Invoke(State);
+            _couldTimeTravel = NewRatCharacter.NewCharacterInput.CanTimeTravel;
             NewRatCharacter.NewCharacterInput.CanTimeTravel = false;
             NewRatCharacter._velocity = Vector3.zero;
             NewRatCharacter.NewCharacterInput.EnableCharacterMovement(false);
@@ -18,7 +20,7 @@ namespace NewRatCharacterController
         }
 
         public override void Exit() {
-            NewRatCharacter.NewCharacterInput.CanTimeTravel = true;
+            NewRatCharacter.NewCharacterInput.CanTimeTravel = _couldTimeTravel;
             NewRatCharacter.AnimationController.SetCaught(false);
             NewRatCharacter.NewCharacterInput.EnableCharacterMovement(true);
         }
