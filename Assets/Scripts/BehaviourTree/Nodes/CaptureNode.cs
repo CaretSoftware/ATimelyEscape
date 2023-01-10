6ff8ad1c79c03739784b85f4530d6c829d222024
August <aggeh@hotmail.se>
2023-01-10 13:14:35 +0100

    replaced scientists

--- Assets/Scripts/BehaviourTree/Nodes/CaptureNode.cs
+++ Assets/Scripts/BehaviourTree/Nodes/CaptureNode.cs
@@ -7,12 +7,15 @@ using UnityEngine.Animations.Rigging;
 
 public class CaptureNode : Node
 {
+    private Vector3 PlayerColliderOffset = new Vector3(0f, 0.1f, 0f);
+
     private Transform player;
     private EnemyAI enemy;
 
     private Vector3 losPos;
     private LayerMask obstacleMask;
     private RaycastHit hit;
+    private Vector3 playerColliderPos;
 
     public CaptureNode(Transform player, EnemyAI enemy, Vector3 losPos, LayerMask obstacleMask)
     {
@@ -24,7 +27,8 @@ public class CaptureNode : Node
 
     public override NodeState Evaluate()
     {
-        Physics.Raycast(losPos, (player.position - losPos).normalized,
+        playerColliderPos = player.position + PlayerColliderOffset;
+        Physics.Raycast(losPos, (playerColliderPos - losPos).normalized,
             out hit, Mathf.Infinity, obstacleMask, QueryTriggerInteraction.Ignore);
         //Debug.Log($"hit layer: {hit.collider.gameObject.layer}");
         if (hit.collider.gameObject.layer == player.gameObject.layer && !enemy.IsCapturing)
