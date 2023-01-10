1a8a7126b5ba217e7ffa30e652a63837fbf51163
Boppan <38586588+Boppan@users.noreply.github.com>
2023-01-10 19:25:58 +0100

    switch on material

--- Assets/Scripts/ChristoffersTestScripts/PuzzleScripts/SwitchOn.cs
+++ Assets/Scripts/ChristoffersTestScripts/PuzzleScripts/SwitchOn.cs
@@ -11,11 +11,15 @@ public class SwitchOn : MonoBehaviour
         [SerializeField] private bool isInteractableByPresent;*/
     [SerializeField] private UnityEvent switchOn;
     [SerializeField] private UnityEvent switchOff;
+    private Material onMaterial;
+    private Material offMaterial;
     private MeshRenderer meshRenderer;
     private bool isOn;
     private Animator animator;
     private void Start()
     {
+        onMaterial = Resources.Load("TestButtonOn") as Material;
+        offMaterial = Resources.Load("M_CB") as Material;
         meshRenderer = GetComponent<MeshRenderer>();
         isOn = true;
         if (isInteractableByPlayer)
@@ -31,6 +35,7 @@ public class SwitchOn : MonoBehaviour
             if (isOn)
             {
                 switchOn.Invoke();
+                meshRenderer.material = onMaterial;
             }
         }
         else if (switchOn != null && isInteractableByPlayer && other.gameObject.tag == "Player")
@@ -38,6 +43,7 @@ public class SwitchOn : MonoBehaviour
             if (isOn)
             {
                 switchOn.Invoke();
+                meshRenderer.material = onMaterial;
                 if (animator != null)
                 {
                     animator.SetBool("On", true);
@@ -62,6 +68,7 @@ public class SwitchOn : MonoBehaviour
             if (isOn)
             {
                 switchOff.Invoke();
+                meshRenderer.material = offMaterial;
             }
         }
         /*        else if (switchOn != null && isInteractableByPast && other.gameObject.tag == "CubePast")
